1cf40d1e2c1914292cf0cabf53a0efd235230506
Nea <eugitoleone@gmail.com>
2018-05-10 04:36:29 +0700

    Nea

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -18,6 +18,26 @@ export class TutorialPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  // สไลด์
+  slides = [
+    {
+      title: "Welcome to the App!",
+      description: "ในแอพพลิเคชั่นนี้มีคำศัพท์มากกว่า 200 คำ และแบ่งเป็น 7 หมวดหมู่",
+      image: "assets/imgs/vocabulary256.png",
+    },
+    {
+      title: "Can you search everytime.",
+      description: "<b>ค้นหาคำศัพท์อย่างรวดเร็ว</b> ทั้งภาษาไทยและภาษาอังกฤษ",
+      image: "assets/imgs/growth.png",
+    },
+    {
+      title: "สามารถอ่านได้ในทุกที่ ทุกเวลา",
+      description: "<b>เพื่อการเรียนรู้และจดจำที่มีประสิทธิภาพ</b> เพราะการเรียนรู้ไม่จำเป็นต้องอยู่แค่เพียงหนังสือ",
+      image: "assets/imgs/idea.png",
+    }
+  ];
+
+  // เชื่อมหน้า
   ionViewDidLoad() {
     console.log('ionViewDidLoad TutorialPage');
   }
